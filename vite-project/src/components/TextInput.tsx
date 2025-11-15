86122c912e2fb8a903f2aaac0dcad2d5e9ad3781
Utkarsh <kutkarsh.umar@gmail.com>
2025-11-15 09:32:25 +0530

    delete TextInput.tsx

--- vite-project/src/components/TextInput.tsx
+++
@@ -1,6 +0,0 @@
-function TextInput() {
-    return <div className="w-full flex flex-col">
-        <label htmlFor="lastname">Last Name <span className="text-green-600">*</span> </label>
-        <input {...register("lastname", { required: "This field is required" })} type="text" name="lastname" id="lastname" />
-    </div>
-}
