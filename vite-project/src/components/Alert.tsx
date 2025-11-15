ce4a00513f4b7e38f21d514cb90818d6a4ed1baa
Utkarsh <kutkarsh.umar@gmail.com>
2025-11-15 11:53:23 +0530

    animation

--- vite-project/src/components/Alert.tsx
+++ vite-project/src/components/Alert.tsx
@@ -3,7 +3,7 @@ import Check from '../assets/images/icon-success-check.svg'
 
 
 function Alert() {
-    return <div className={`absolute bg-green-600 text-white p-5 rounded-xl flex flex-col gap-2 top-2.5 w-[90%] max-w-[500px] transition-opacity`}>
+    return <div className={`absolute bg-green-600 text-white p-5 rounded-xl flex flex-col gap-2 top-2.5 w-[90%] max-w-[500px] animate-appear opacity-0`}>
         <div className='flex gap-2 items-center'>
             <img src={Check} alt="tick image" />
             <p className='font-semibold text-[16px]'>Message Sent!</p>
